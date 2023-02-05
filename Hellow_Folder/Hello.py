3b205ecb7e6814e4c3f3e45cebad6142c6130625
Adams Mutai <adams.tebes@gmail.com>
2023-02-04 19:14:31 -0500

    Rename Hello folder to Hello_ Folder

--- Hellow_Folder/Hello.py
+++ Hellow_Folder/Hello.py
@@ -1,6 +1,12 @@
+
 print("third try")
 print("everything looks to be all set")
 print("making multiple commits pt1")
 print("making multiple commits pt2")
 print("moree")
 print("even more")
+print("commit")
+print("commit 2")
+print("commit")
+def commitFuntion():
+    print("all prints finalized")
